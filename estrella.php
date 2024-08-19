9595560a26151a907298519a65d02560043b2a67
omyfreyre123 <krispo475@gmail.com>
2024-08-19 17:14:25 -0600

    commit

---
+++ estrella.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Estrella Amairani Bonilla Lechuga">
+    <title>Sobre Estrella</title>
+
+    <!-- El orden importa al agregar librerías -->
+    <!-- Normalize va al inicio -->
+    <link rel="preload" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    
+    <!-- Mejorar el performance -->
+    <link rel="preload" href="css/estilos.css" as="style">   
+    <link rel="stylesheet" href="css/estilos.css">   
+</head>
+<body>
+    
+    <header>
+        <h1>Sobre Estrella</h1>
+    </header>
+
+    <div class="nav-bg">
+    <nav class="navegacion-principal contenedor">
+            <a href="index.php">Inicio</a>
+            <a href="sobremi.php">Sobre Mi</a>
+            <a href="clientes.php">Clientes</a>
+            <a href="contacto.php">Contacto</a>
+            <a href="colaborador.php">colaborador omar</a>
+            <a href="dariana.php">DESARROLLADOR DARIANA</a>
+            <a href="estrella.php">DISEÑADORA ESTRELLA</a>
+            <a href="admin-omar/login.php">Iniciar Sesion</a>
+        </nav>
+    </div>
+
+    <main class="contenedor sombra">
+        <section class="sobre-mi">
+            <h2>Conoce a Estrella, una diseñadora front-end apasionada</h2>
+            <p>Estrella Amairani Bonilla Lechuga es una talentosa diseñadora front-end con base en Nanchital, Veracruz. Con una profunda pasión por el diseño y la tecnología, Estrella se especializa en crear interfaces de usuario intuitivas y atractivas que mejoran la experiencia del usuario y cumplen con los objetivos del negocio.</p>
+            
+            <p>Desde el inicio de su carrera en el diseño gráfico, Estrella ha estado fascinada por el poder del diseño para influir en la percepción y la interacción del usuario. Esta fascinación la llevó a especializarse en el desarrollo front-end, donde combina su experiencia en diseño visual con conocimientos técnicos en HTML, CSS y JavaScript para construir sitios web funcionales y estéticamente agradables.</p>
+
+            <p>Estrella está siempre al tanto de las últimas tendencias en diseño web y tecnologías emergentes. Su enfoque está en la creación de experiencias digitales que no solo sean visualmente impactantes, sino también accesibles y eficientes. Además, es experta en trabajar con herramientas de diseño modernas y en la implementación de principios de diseño responsive para asegurar que sus proyectos se vean bien en cualquier dispositivo.</p>
+
+            <p>En su tiempo libre, Estrella disfruta explorando nuevas tendencias de diseño, participando en comunidades de diseño y desarrollo, y colaborando en proyectos creativos que le permiten experimentar y aprender. Su objetivo es seguir creciendo como diseñadora y ofrecer siempre soluciones de diseño innovadoras y efectivas a sus clientes.</p>
+        </section>
+    </main>
+
+</body>
+</html>
